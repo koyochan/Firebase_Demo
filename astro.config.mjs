4e276e542e9634c01fe2ec01512081daa170ef58
koyochan <121277745+koyochan@users.noreply.github.com>
2025-03-02 19:20:58 +0900

    siteを変動値で認識できるように、astro.config.mjsを変更しました

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
 	output: "server",
   	adapter: vercel(),
 	vite: {
